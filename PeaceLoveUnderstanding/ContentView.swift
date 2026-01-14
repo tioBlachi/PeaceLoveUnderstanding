ea1ce2f59690ab1717eb45446b54fd9d7f120e5c
Blas Antunez <blasantunez310@gmail.com>
2026-01-13 23:03:55 -0500

    Initial Commit

---
+++ PeaceLoveUnderstanding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PeaceLoveUnderstanding
+//
+//  Created by Blas Antunez on 1/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PeaceLoveUnderstanding/PeaceLoveUnderstandingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeaceLoveUnderstandingApp.swift
+//  PeaceLoveUnderstanding
+//
+//  Created by Blas Antunez on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct PeaceLoveUnderstandingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
