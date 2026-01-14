a6a6cb0e514d88c7ade04e2e11a2067603ee3b1e
Blas Antunez <blasantunez310@gmail.com>
2026-01-13 23:39:08 -0500

    Completed work for Peace Love Understanding assignment from Prof Gallaugher Switf UI course

--- PeaceLoveUnderstanding/ContentView.swift
+++ PeaceLoveUnderstanding/ContentView.swift
@@ -8,12 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var imageName = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's So Funny 'Bout")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+            
+            Spacer()
+            
+            VStack {
+                Image(systemName: imageName)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(message)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundStyle(.purple)
+            }
+            .frame(width: 300, height: 300)
+            
+            Spacer()
+            
+            HStack {
+                Button("Peace") {
+                    imageName = "peacesign"
+                    message = "Peace"
+                }
+                Button("Love") {
+                    imageName = "heart"
+                    message = "Love"
+                }
+                Button("Understanding") {
+                    imageName = "lightbulb"
+                    message = "Understanding"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(Color.purple)
         }
         .padding()
     }
